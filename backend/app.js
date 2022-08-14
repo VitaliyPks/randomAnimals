e83f26593204530a313afa7a37c62e963bd0bb33
Vitaliy <vetal9305@gmail.com>
2022-08-14 18:01:13 +0300

    add route *

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const config = require('./config/config');
 const catApiRouter = require('./routes/cat.api.router');
 const dogApiROuter = require('./routes/dog.api.router');
 const foxApiRouter = require('./routes/fox.api.router');
-const insultApiRouter = require('./routes/insult.api.router')
+const insultApiRouter = require('./routes/insult.api.router');
 
 const app = express();
 
@@ -15,6 +16,10 @@ app.use('/api/cat', catApiRouter);
 app.use('/api/dog', dogApiROuter);
 app.use('/api/fox', foxApiRouter);
 app.use('/api/insult', insultApiRouter);
+
+app.use('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
 
 app.listen(port, () => {
   console.log(` Server started at ${port} port`);
