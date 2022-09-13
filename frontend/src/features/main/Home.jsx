8156f82cf525ecd93213c02bd7ce7cf35858660b
Vitaliy <vetal9305@gmail.com>
2022-09-13 12:25:44 +0300

    fix home text

--- frontend/src/features/main/Home.jsx
+++ frontend/src/features/main/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   document.title = 'Random animals'
   return (
     <div className="bg-gray-500/90 items-center mt-20 m-auto border max-w-xl py-2 px-4 rounded-lg flex flex-col items-center mb-2 text-white">
-      <h1 className='m-5 text-xl'>Добро пожаловать на наш сайт.<br /><br />Здесь Вы можете посмотреть на случайную подборку различныйх животных и не только...
+      <h1 className='m-5 text-xl'>Добро пожаловать на наш сайт.<br /><br />Здесь Вы можете посмотреть на случайную подборку различных животных и не только...
       </h1>
     </div>
   )
